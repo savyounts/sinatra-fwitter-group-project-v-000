540673e9f1b0a142c639a583fdfa0c731506d28d
Savannah Younts <savyounts@gmail.com>
2018-09-13 17:35:04 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,11 +14,11 @@ class ApplicationController < Sinatra::Base
 
   helpers do
     def logged_in?
-      !!session[:user_id]
+      current_user != nil
     end
 
     def current_user
-      User.find(session[:user_id])
+      @current_user ||= User.find(session[:user_id])
     end
   end
 
