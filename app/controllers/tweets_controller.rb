f701e9038e8af8137bdb665d471e2abf736c93f7
Savannah Younts <savyounts@gmail.com>
2018-09-13 18:56:03 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,12 @@
 class TweetsController < ApplicationController
 
   get '/tweets' do
-    @tweets = Tweet.all
-    erb :'/tweets/tweets'
+    if logged_in?
+      @tweets = Tweet.all
+      erb :'/tweets/tweets'
+    else 
+      redirect '/login'
+    end
   end
 
   get '/tweets/new' do
