a3dc2e8c41a05c4b632c9c27acf42bba606e6c23
Savannah Younts <savyounts@gmail.com>
2018-09-13 14:14:53 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,29 @@ class UsersController < ApplicationController
   get '/' do
     erb :index
   end
+
+  get '/users/create_user' do 
+
+    erb :'/users/create_user'
+  end 
+
+  post '/users/create_users' do 
+
+  end 
+
+  get '/users/login' do 
+
+    erb :'/users/login'
+  end 
+
+  post '/users/login' do 
+
+  end 
+
+  get '/users/:id'
+
+    erb :'/users/show'
+  end 
+
+  
 end
