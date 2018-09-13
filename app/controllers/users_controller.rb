80c2d030d458dfd625408cc5edbfad183048bade
Savannah Younts <savyounts@gmail.com>
2018-09-13 18:45:47 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
 
   post '/login' do
      @user = User.find_by(username: params[:username])
-     binding.pry
+    #  binding.pry
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect "/users/#{@user.id}"
