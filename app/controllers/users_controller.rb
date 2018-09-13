649eea248c3f77c0a6e6aa07d53bdf74c4472e33
Savannah Younts <savyounts@gmail.com>
2018-09-13 16:42:13 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,8 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-    @user = User.create(username: params[:username], email: params[:email], password: params[:password])
-    if @user
+    if !params[:username].empty?
+      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
       session[:user_id] = @user.id
       redirect "/tweets"
     else
