8050454494a3e30fb40ea4b8a549c30b72574cdf
Savannah Younts <savyounts@gmail.com>
2018-09-13 17:13:35 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,16 +45,5 @@ class UsersController < ApplicationController
 
 
 
-  helpers do
-    def logged_in?
-      !!session[:user_id]
-    end
-
-    def current_user
-      User.find(session[:user_id])
-    end
-  end
-
-
 
 end
