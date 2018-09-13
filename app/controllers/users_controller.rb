c9688549f8a10317cf059b19390f1d8fbd5d2400
Savannah Younts <savyounts@gmail.com>
2018-09-13 16:55:51 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,11 @@ class UsersController < ApplicationController
   end
 
   get '/signup' do
+    if logged_in?
+      redirect '/tweets'
+    else
       erb :'/users/create_user'
+    end
   end
 
   post '/signup' do
