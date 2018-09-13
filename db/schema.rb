df7af18531b2c9616c7ebab083b8ae82b59607f1
Savannah Younts <savyounts@gmail.com>
2018-09-13 14:48:16 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180912223721) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
