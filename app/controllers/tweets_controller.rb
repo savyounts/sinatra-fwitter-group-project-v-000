f13f5ad6258ddd4e3926ed6b9e5362da826ac887
Savannah Younts <savyounts@gmail.com>
2018-09-13 14:24:23 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -11,27 +11,27 @@ class TweetsController < ApplicationController
   end
 
   post '/tweets' do
-
+    @tweet = Tweet.create(params)
     redirect '/tweets/:id'
   end
 
-  get '/tweets/edit' do
-
+  get '/tweets/:id/edit' do
+    @tweet = Tweet.find(params[:id])
     erb :'/tweets/edit'
   end
 
   patch '/tweets/:id' do
-
+    @tweet = Tweet.find(params[:id])
     redirect "/tweets/#{@tweet.id}"
   end
 
   get '/tweets/:id' do
-
+    @tweet = Tweet.find(params[:id])
     erb :'tweets/show'
   end
 
   delete '/tweets/:id/delete' do
-
+    @tweet = Tweet.find(params[:id])
     @tweet.destroy
     redirect '/tweets'
   end
