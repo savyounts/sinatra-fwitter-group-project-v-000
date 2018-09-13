ba40919680cafbcdf4412c8924a4cd1400ba3189
Savannah Younts <savyounts@gmail.com>
2018-09-13 17:21:43 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
       end
 
       def current_user
-        User.find(session[:user_id]) ? true : false
+        User.find(session[:user_id])
       end
     end
 
