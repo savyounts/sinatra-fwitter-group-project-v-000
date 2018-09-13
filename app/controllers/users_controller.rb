34ebd05c7b7fcb532bcfcbf5fa0970241706cf12
Savannah Younts <savyounts@gmail.com>
2018-09-13 19:06:49 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
      @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect "/users/#{@user.id}"
+      redirect "/tweets"
     else
       redirect '/login'
     end
