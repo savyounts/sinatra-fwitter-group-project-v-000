9a2f5f6a5d56e0fe41863cddc3700581f1306439
Savannah Younts <savyounts@gmail.com>
2018-09-13 18:48:00 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
-    enable :sessions,
+    enable :sessions
     set :session_secret, "secret"
     set :public_folder, 'public'
     set :views, 'app/views'
