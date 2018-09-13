fb551de3d39d748b3310cd05cc4359687cc0c574
Savannah Younts <savyounts@gmail.com>
2018-09-13 19:09:59 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ActiveRecord::Base
   validates :username, uniqueness: true
   validates :username, presence: true
   validates :email, presence: true
+
+  def slug 
+    name.split(" ").join("-")
+  end
 end
