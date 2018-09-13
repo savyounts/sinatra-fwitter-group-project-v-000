4dc2c09575ade632e40a058af7b03dcca2a59602
Savannah Younts <savyounts@gmail.com>
2018-09-13 14:21:01 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,34 @@
 class TweetsController < ApplicationController
 
+  get '/tweets/new' do
+
+    erb :'/tweets/new'
+  end
+
+  post '/tweets' do
+
+    redirect '/tweets/:id'
+  end
+
+  get '/tweets/edit' do
+
+    erb :'/tweets/edit'
+  end
+
+  patch '/tweets/:id' do
+
+    redirect "/tweets/#{@tweet.id}"
+  end
+
+  get '/tweets/:id' do
+
+    erb :'tweets/show'
+  end
+
+  delete '/tweets/:id/delete' do
+
+    @tweet.destroy
+    redirect '/tweets'
+  end 
 
 end
