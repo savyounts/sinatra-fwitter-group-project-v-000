a1c18712b48731d8baa8516ac166b89197963c8b
Savannah Younts <savyounts@gmail.com>
2018-09-13 15:29:58 +0000

    automatically backed up by learn

--- db/seed.rb
+++ db/seed.rb
@@ -1,8 +1,8 @@
-20.times do 
-    User.create(username: Faker::Name, email: Faker::Internet, password: Faker::Artist)
+20.times do
+    User.create(username: Faker::Name.name, email: Faker::Internet.email, password: Faker::NewGirl.character)
 end
 
-100.times do 
+100.times do
   rando_user_id = rand(1..20)
   Tweet.create(content: Faker::ChuckNorris.fact, user_id: rando_user_id)
 end
